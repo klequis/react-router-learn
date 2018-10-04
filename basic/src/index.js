bc3402c9b68add6c6753b6bacbe7ca4563a7c683
klequis <klequis@gmail.com>
2018-10-03 19:14:58 -0700

    add basic example

---
+++ basic/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import BasicExample from './BasicExample'
+
+ReactDOM.render(
+  <BasicExample />,
+  document.getElementById('root')
+)
